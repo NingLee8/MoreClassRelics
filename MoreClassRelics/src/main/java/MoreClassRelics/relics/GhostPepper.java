438f389cf88871feb6199cfa84b7e5824d6511a8
Ning <ninglee8@gmail.com>
2021-10-30 15:17:08 -0700

    Default mod base commit

---
+++ MoreClassRelics/src/main/java/MoreClassRelics/relics/GhostPepper.java
@@ -0,0 +1,42 @@
+package MoreClassRelics.relics;
+
+import MoreClassRelics.DefaultMod;
+import MoreClassRelics.util.TextureLoader;
+import basemod.abstracts.CustomRelic;
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
+
+import static MoreClassRelics.DefaultMod.makeRelicOutlinePath;
+import static MoreClassRelics.DefaultMod.makeRelicPath;
+
+public class GhostPepper extends CustomRelic {
+    public static final String ID = DefaultMod.makeID("GhostPepper");
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("ghost_pepper.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("ghost_pepper.png"));
+    private static final int HP_AMT = 16;
+
+    public GhostPepper() {
+        super(ID, IMG, OUTLINE, RelicTier.SHOP, LandingSound.FLAT);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return this.DESCRIPTIONS[0];
+    }
+
+    @Override
+    public void onEquip() {
+        AbstractDungeon.player.damage(new DamageInfo(AbstractDungeon.player, HP_AMT));
+        AbstractDungeon.effectList.add(new FlashAtkImgEffect(AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY, AbstractGameAction.AttackEffect.FIRE));
+        AbstractDungeon.player.increaseMaxHp(HP_AMT, true);
+    }
+
+    @Override
+    public AbstractRelic makeCopy() {
+        return new GhostPepper();
+    }
+}
