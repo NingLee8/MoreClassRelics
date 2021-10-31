431294f06152e597bf193ad1ea8b298e6e0f9df7
Ning <ninglee8@gmail.com>
2021-10-30 20:25:21 -0700

    Black Mirror Relic (with relic)

---
+++ MoreClassRelics/src/main/java/MoreClassRelics/relics/BlackMirror.java
@@ -0,0 +1,30 @@
+package MoreClassRelics.relics;
+
+import MoreClassRelics.DefaultMod;
+import MoreClassRelics.util.TextureLoader;
+import basemod.abstracts.CustomRelic;
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+import static MoreClassRelics.DefaultMod.makeRelicOutlinePath;
+import static MoreClassRelics.DefaultMod.makeRelicPath;
+
+public class BlackMirror extends CustomRelic {
+    public static final String ID = DefaultMod.makeID("BlackMirror");
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("ghost_pepper.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("ghost_pepper.png"));
+
+    public BlackMirror() {
+        super(ID, IMG, OUTLINE, RelicTier.UNCOMMON, LandingSound.MAGICAL);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return this.DESCRIPTIONS[0];
+    }
+
+    @Override
+    public AbstractRelic makeCopy() {
+        return new BlackMirror();
+    }
+}
